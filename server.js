8f0ef746ff7337f10c331ef6917dd61e6b4e267f
Khudd1214 <77385940+Khudd1214@users.noreply.github.com>
2021-12-26 17:23:36 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.listen(listeningPort, function (err) {
   console.log("Server started on " + listeningPort);
 });
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/homepage.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/homepage.html");
 });
 
 app.post("/", function (req, res) {
@@ -25,11 +25,48 @@ app.post("/", function (req, res) {
   if (FSUtilitiesSync.findDB(currentDBPath) === false) {
     res.send(currentDBPath);
   } else {
-    let result = FSUtilitiesSync.searchDB(
+    let result = FSUtilitiesSync.verifyUser(
       userInput[0],
       userInput[1],
       currentDBPath
     );
     res.send(result);
   }
+});
+
+app.get("/registration", function (req, res) {
+  res.sendFile(__dirname + "/registration.html");
+});
+
+app.post("/registration", function (req, res) {
+  const body = req.body;
+  currentDBPath = __dirname + "\\userData.txt";
+  let result = null;
+  const userInput = [
+    req.body.username,
+    req.body.password,
+    req.body.firstName,
+    req.body.lastName,
+    req.body.email,
+    req.body.phoneNumber,
+  ];
+  if (FSUtilitiesSync.findUser(userInput[0], currentDBPath) !== false) {
+    res.send("Sorry, this user already exists.");
+  } else {
+    FSUtilitiesSync.addUser(
+      userInput[0],
+      userInput[1],
+      userInput[2],
+      userInput[3],
+      userInput[4],
+      userInput[5],
+      currentDBPath
+    );
+    result = FSUtilitiesSync.verifyUser(
+      userInput[0],
+      userInput[1],
+      currentDBPath
+    );
+  }
+  res.send(result);
 });
